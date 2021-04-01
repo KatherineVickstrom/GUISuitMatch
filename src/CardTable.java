4942c07316bf28f62fb7839a8593962ff92fc87d
Michael Hackett <mhackett909@gmail.com>
2021-04-01 11:06:11 -0700

    Blueprint

--- src/CardTable.java
+++ src/CardTable.java
@@ -1,3 +1,12 @@
-public class CardTable {
-    
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CardTable extends JFrame {
+    static int MAX_CARDS_PER_HAND = 56;
+    static int MAX_PLAYERS = 2;  // for now, we only allow 2 person games
+
+    private int numCardsPerHand;
+    private int numPlayers;
+
+    public JPanel pnlComputerHand, pnlHumanHand, pnlPlayArea;
 }
